22161535352b53bb9627ff7f9c976ad512be97d0
Bissmar <bischavez86@gmail.com>
2021-06-30 00:51:38 -0400

    Conversión de la pl antilla Simple Sidebar Startbootstrap en la plantilla del apartado del proyecto

---
+++ src/app/@admin/core/components/header/header.component.ts
@@ -0,0 +1,26 @@
+import { Component, Output, EventEmitter } from '@angular/core';
+
+@Component({
+  selector: 'app-admin-header',
+  templateUrl: './header.component.html',
+  styleUrls: ['./header.component.scss']
+})
+export class HeaderComponent {
+  toggledValue = true;
+  @Output() toggleChange = new EventEmitter<boolean>();
+
+ toggle(){
+   if (this.toggledValue === undefined){
+     this.toggledValue = true;
+
+   }
+   this.toggledValue = !this.toggledValue;
+   console.log(this.toggledValue);
+   this.toggleChange.emit(this.toggledValue);
+
+
+ }
+
+  
+
+}
